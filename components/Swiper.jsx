50c85965f07ff7e282b58ef16a78075c29316ad5
AmithSunil <crispyfingers99@gmail.com>
2023-10-30 20:33:57 +0530

    Ippa

--- components/Swiper.jsx
+++ components/Swiper.jsx
@@ -9,17 +9,68 @@ import styles from './Swiper.module.css';
 
 import { Navigation } from 'swiper/modules';
 
-const slides = [<SwiperCard />, <SwiperCard />, <SwiperCard />, <SwiperCard />];
-
+const slides = [
+  {
+    title: 'Slide 1',
+    location: 'Location 1',
+    price: 'Price 1',
+  },
+  {
+    title: 'Slide 2',
+    location: 'Location 2',
+    price: 'Price 2',
+  },
+  {
+    title: 'Slide 3',
+    location: 'Location 3',
+    price: 'Price 3',
+  },
+  {
+    title: 'Slide 4',
+    location: 'Location 4',
+    price: 'Price 4',
+  },
+  {
+    title: 'Slide 5',
+    location: 'Location 5',
+    price: 'Price 5',
+  },
+  {
+    title: 'Slide 6',
+    location: 'Location 6',
+    price: 'Price 6',
+  },
+  {
+    title: 'Slide 7',
+    location: 'Location 7',
+    price: 'Price 7',
+  },
+  {
+    title: 'Slide 8',
+    location: 'Location 8',
+    price: 'Price 8',
+  },
+  {
+    title: 'Slide 9',
+    location: 'Location 9',
+    price: 'Price 9',
+  },
+  {
+    title: 'Slide 10',
+    location: 'Location 10',
+    price: 'Price 10',
+  },
+];
+  
 export default function App() {
   return ( 
-        <>   
+    <>   
       <Swiper navigation={true} modules={[Navigation]} className="mySwiper">
-        {slides.map((slideContent, index) => {
+        {slides.map((slide, index) => {
           return (
             <SwiperSlide className={styles.swiperslide} key={index}>
-              <div className={styles.swipercard}>{slideContent}
-                
+              <div className={styles.swipercard}>
+                <SwiperCard title={slide.title} price={slide.price} location={slide.location} />
               </div>
             </SwiperSlide>
           );
--- components/SwiperCard.jsx
+++ components/SwiperCard.jsx
@@ -4,8 +4,10 @@ const SwiperCard=({ imageSrc, title, price, location })=> {
     return (
         <div className="card">
             <div className="card-image">
-                <Image src={imageSrc} alt={title} width={300} height={200} />
-            </div>
+            <img
+          src="https://firebasestorage.googleapis.com/v0/b/destinations-6c6a6.appspot.com/o/pexels-spencer-davis-4388164.jpg?alt=media&token=2f97038b-8b1b-46ac-bcc0-0069efef7e3d"
+          alt=""
+        />            </div>
             <div className="card-content">
                 <h2>{title}</h2>
                 <p>{location}</p>
